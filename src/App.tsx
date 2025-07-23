28377bc9f9b083c8c588cd79c93b56dc7fa0c642
Silantiev Jan <Silantiev-999@mail.ru>
2025-07-23 21:52:25 +0300

    relis2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import './App.scss';
-import { TodoList } from './pages/Pages';
+import { Input } from './components/Input/Input';
+import { Button } from './components/Button/Button';
 
 const App = () => {
+    const [taskName, setTasksName] = useState("");
+    const [taskValue, setTasksValue] = useState("");
+    const [objectData, setObjectData] = useState({});
+  
+  const handleAddTask = () => {
+    if(!taskName.trim()) return
+  
+    setObjectData(prevData => ({
+            ...prevData, 
+            [taskName]: taskValue || '' 
+        }));
+    setTasksName('')
+    setTasksValue('')
+    console.log()
+  }
+  console.log(objectData)
   return (
     <div className="container">
-      <TodoList />
+      <Input
+        label="Название поля:"
+        name={"Название поля"}
+        onChange={(e) => {setTasksName(e.target.value)}}
+        value={taskName}
+      />
+      <Input
+        label="Значение поля:"
+        name={"Значение поля"}
+        onChange={(e) => setTasksValue(e.target.value)}
+        value={taskValue}
+      />
+      <Button title="Создать поле в обьекте" onClick={handleAddTask} />
+      <div className='object'> 
+        <p>Обьект: {JSON.stringify(objectData,null, 3)}</p></div>
+
     </div>
   );
 };
